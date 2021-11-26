a208f679906b5d2526f41ba84f3a3ed85d24b82b
Larry Salomon <salomonlarry@gmail.com>
2021-11-26 22:19:20 +0100

    New funcion for palindrome sentences

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,14 @@
 def is_palindrome(string):
-    return string == string[::-1]
+    return string.lower() == string[::-1].lower()
 
-print(is_palindrome("pop"))
+print(is_palindrome("poP"))
+
+import string
+
+def is_palindrome_s(str):
+    whitelist = set(string.ascii_lowercase)
+    str = str.lower()
+    str = ''.join([char for char in str if char in whitelist])
+    return str == str[::-1]
+
+print(is_palindrome_s('A dog! A panic in a pagoda'))
