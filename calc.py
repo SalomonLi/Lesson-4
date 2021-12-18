92216646a04a4e4be9e0aceef9e4762f86c90701
Larry Salomon <salomonlarry@gmail.com>
2021-12-18 14:55:45 +0100

    choices

--- calc.py
+++ calc.py
@@ -9,7 +9,7 @@ _help = "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 
 parser = argparse.ArgumentParser()
 
 def parsers():
-    parser.add_argument('operations', help=_help, type=int)
+    parser.add_argument('operations', help=_help, type=int, choices=[1, 2, 3, 4])
     parser.add_argument('first_number', help="Enter number one.", type=int)
     parser.add_argument('two_number', help="Enter number two.", type=int)
     args = parser.parse_args()
@@ -19,7 +19,7 @@ def calc(args):
     try:
         print("Rezultat",keys_job[args.operations](args.first_number, args.two_number))
     except ZeroDivisionError:
-        print("nie mozna rozdelic na 0 ")
+        print("nie można rozdelic na 0 ")
     except KeyError:
         print(_help)
 
@@ -27,38 +27,13 @@ def calc(args):
 
 if __name__ == "__main__":
     calc(parsers())
+    # print(parsers())
     while True:
         io = input("Continue Y / N\n").lower()
-        if "n" == io:
+        if io == "n":
             exit()
         keys = int(input(f"{_help}\n"))
         a = int(input("Podaj perwszom liczbę:\n"))
         b = int(input("Podaj drugom liczbę:\n"))
-        print("Rezultat\n",keys_job[keys](a, b))
+        print("Rezultat\n", keys_job[keys](a, b))
 
-
-
-
-
-
-
-
-    # if len(sys.argv) > 1 < 3:
-    #     # if sus.argv[1] == int and sus.argv[2] == int and sus.argv[3] == int:
-    #     keys, a, b = sys.argv[1:]
-    #     keys_job[keys](a,b)
-    #
-    # elif len(sys.argv) > 3:
-    #     print("Zaduzo argumentuv")
-    # elif len(sys.argv) <= 1:
-    #     print("nie bylo podane zadnyh argumentuw do calkulatora")
-    #
-    # while True:
-    #     io = input("Continue Y / N\n").lower()
-    #     if "n" == io:
-    #         exit()
-    #     keys = int(input(
-    #         "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:\n"))
-    #     a = int(input("Podaj perwszom liczbę:\n"))
-    #     b = int(input("Podaj drugom liczbę:\n"))
-    #     keys_job[keys](a,b)
