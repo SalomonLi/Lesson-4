a6a0eb4ba60019d071c4489676bb10f57ad9aace
Larry Salomon <salomonlarry@gmail.com>
2021-12-21 20:55:36 +0100

    def user input

--- calc.py
+++ calc.py
@@ -6,35 +6,45 @@ keys_job = {1 : operator.add, 2 : operator.sub, 3 : operator.mul, 4 : operator.t
 
 _help = "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"
 
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('operations', help=_help, type=int, choices=[1, 2, 3, 4])
+    parser.add_argument('first_number', help="Enter number one.", type=int)
+    parser.add_argument('two_number', help="Enter number two.", type=int)
+    args = parser.parse_args()
+    parsers_resul = args.operations, args.first_number, args.two_number
+    return parsers_resul
 
-parser = argparse.ArgumentParser()
-parser.add_argument('operations', help=_help, type=int, choices=[1, 2, 3, 4])
-parser.add_argument('first_number', help="Enter number one.", type=int)
-parser.add_argument('two_number', help="Enter number two.", type=int)
-args = parser.parse_args()
-
-
-def calc2(operation, a, b):
+def calc2(args):
+    operation, a, b = args
     operator = keys_job[operation]
-    try:
-     result = operator(a, b)
-     print(result)
-    except ZeroDivisionError:
-        print("nie można rozdelic na 0 ")
-    except KeyError:
-        print(_help)
-    return
+    result = operator(a, b)
+    return result
+
+def input_users():
+    operations = int(input(f"{_help}\n"))
+    first_number = int(input("Podaj perwszom liczbę:\n"))
+    two_number = int(input("Podaj drugom liczbę:\n"))
+    resul = operations, first_number, two_number
+    return resul
+
+def main():
+    print(calc2(parser()))
+    while True:
+        io = input("Continue Y / N\n").lower()
+        if io.lower() == "help":
+            print(_help)
+            continue
+        if io.lower() in {"quit", "exit", "n"}:
+            raise SystemExit()
+        try:
+            print(calc2(input_users()))
+        except ZeroDivisionError:
+            print("nie mozna rozdelic na 0")
+        continue
 
 
 if __name__ == "__main__":
-    calc2(args.operations, args.first_number, args.two_number)
-    while True:
-        io = input("Continue Y / N\n").lower()
-        if io == "n":
-            exit()
-        operations = int(input(f"{_help}\n"))
-        first_number = int(input("Podaj perwszom liczbę:\n"))
-        two_number = int(input("Podaj drugom liczbę:\n"))
-        calc2(operations, first_number, two_number)
+    main()
 
 
